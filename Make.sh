529354ea9804fcaaaf396f189788c814bcde9ed4
zhengxin <zx358317301@qq.com>
2020-08-24 13:58:08 +0800

    修改

--- Make.sh
+++ Make.sh
@@ -1,3 +1,1 @@
-docker run --mount type=bind,source=$(pwd),target=/srv/jekyll \
--p 4000:4000 --name blog --rm -it jekyll/jekyll \
-jekyll serve
+docker run --volume=$(pwd)/srv/jekyll -p 4000:4000 --name blog --rm -it jekyll/jekyll jekyll serve
