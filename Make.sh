60e72cea3785a313082a4ae1031917cadbdf333d
zhengxin <zx358317301@qq.com>
2020-08-24 15:15:24 +0800

    修改

--- Make.sh
+++ Make.sh
@@ -1,1 +1,2 @@
-docker run --rm -d -v "$(pwd):/src" -p 4000:4000 grahamc/jekyll serve -H 0.0.0.0
+docker stop zx-blog
+docker run --name zx-blog --rm -d -v "$(pwd):/src" -p 4000:4000 grahamc/jekyll serve -H 0.0.0.0
