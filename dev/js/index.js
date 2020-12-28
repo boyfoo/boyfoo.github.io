85b895937d0aad54193fdd4f9aa9af9e269c1280
zhengxin <zx358317301@qq.com>
2020-12-28 16:26:45 +0800

    修改

--- dev/js/index.js
+++ dev/js/index.js
@@ -241,7 +241,7 @@ $(document).ready(function(){
     }
     $('.post-content').on('mouseup', function(e) {
         var txt = window.getSelection();
-        if (txt.toString().length >= 30) {
+        if (txt.toString().length >= 1) {
             setClipboardData(txt);
         }
     });
